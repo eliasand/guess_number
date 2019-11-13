90ba1f57ede96c5c314d229c82344e838ef6ce65
eliasand <elias.andersson2@elev.ga.ntig.se>
2019-11-13 12:38:23 +0100

    färdigt spel

--- guess_numb.py
+++ guess_numb.py
@@ -1,34 +1,45 @@
-import math, random
+import math, random, time
 
 gameLoop = False
 startLoop = True
-
-start = input("välkommen till ett gissa talet spel.\nÄr du redo att spela? [JA] eller [NEJ]: ")
+restartLoop = True
 
 while startLoop == True:
-	
-	if start == "ja" or "JA":
+	start = input("välkommen till ett gissa talet spel.\nÄr du redo att spela? [JA] eller [NEJ]: ").lower()
+	if start == "ja":
 		gameLoop = True
 		startLoop = False
-	elif start == "nej" or "NEJ":
+	elif start == "nej":
 		gameLoop = False
 		startLoop = False
+		print("avslutar programmet...")
+		time.sleep(2)
 	else:
-		start = input("Ditt svar var inte accepterat av systemet\nÄr du redo att spela? [JA] eller [NEJ]: ")
-		pass
+		print("Ditt svar var inte accepterat av systemet.")
 
 answer = random.randint(1,10)
 while gameLoop == True:
 	try:	
 				
 		guess = int(input("Bra.\nVälj ett heltal mellan 1 och tio: "))
-		print(answer, guess)
 		if guess > answer:
 			print("Ditt tal var för stort, prova igen.")
 		elif guess < answer:
 			print("Ditt tal var för litet, prova igen.")
 		elif guess == answer:
 			print("Rätt svar var " + str(answer) + ", bra jobbat.")
-			gameLoop = False
+			while restartLoop == True:
+				restart = input("Vill du spela igen? [JA] eller [NEJ]: ").lower()
+				if restart == "ja":
+					restartLoop = False
+				elif restart == "nej":
+					gameLoop = False
+					restartLoop = False
+					print("avslutar programmet...")
+					time.sleep(2)
+				else:
+					print("Ditt svar var inte accepterat av systemet.")
+					restartLoop = True
+
 	except:
 		print("Något gick fel.")
